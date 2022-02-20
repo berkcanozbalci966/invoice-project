dd4c84a6dc1e2768c8cf0bddf9f1b5fd02e5dad0
unknown <berkcanozbalci@gmail.com>
2022-02-20 20:58:58 +0300

    added manytomanyfield in invoices module

--- invoices/models.py
+++ invoices/models.py
@@ -5,6 +5,13 @@ from receivers.models import Receiver
 # Create your models here.
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return  str(self.name)
+
+
 class Invoice(models.Model):
     profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
     receiver =  models.ForeignKey(Receiver,on_delete=models.CASCADE)
@@ -15,6 +22,8 @@ class Invoice(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     # if closed - hide add positions and ubable adding new ones
     closed = models.BooleanField(default=False)
+    # for educational purposes only
+    tag = models.ManyToManyField(Tag,blank=True)
 
     def __str__(self):
         return f"Invoice number: {self.number}, pk: {self.pk}"
