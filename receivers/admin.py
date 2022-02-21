5abe2ca2accb7a3612c23c171ffd289c3c7841b8
unknown <berkcanozbalci@gmail.com>
2022-02-21 11:36:56 +0300

    feat: receivers export created

--- receivers/admin.py
+++ receivers/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from .models import Receiver
+
+from import_export import resources
+from import_export.admin import ExportActionMixin
+
 # Register your models here.
 
-admin.site.register(Receiver) 
+class ReceiverResource(resources.ModelResource):
+    class Meta:
+        model = Receiver
+        fields = ('id','name','address','website','created')
+        export_order = ('website','created','address','name','id')
+
+
+class ReceiverAdmin(ExportActionMixin,admin.ModelAdmin):
+    resource_class = ReceiverResource
+
+admin.site.register(Receiver,ReceiverAdmin) 
